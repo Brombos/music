a98b97211f9ddc1b076bd1b221214f8975359d28
Brombos <ddaandan.2013@gmail.com>
2024-01-31 20:58:59 +0100

    New

---
+++ radio/test.py
@@ -0,0 +1,20 @@
+from git import Repo
+import os
+
+origin = empty_repo.create_remote("origin", repo.remotes.origin.url)
+assert origin.exists()
+assert origin == empty_repo.remotes.origin == empty_repo.remotes["origin"]
+origin.fetch()  # assure we actually have data. fetch() returns useful information
+# Setup a local tracking branch of a remote branch
+empty_repo.create_head("master", origin.refs.master)  # create local branch "master" from remote "master"
+empty_repo.heads.master.set_tracking_branch(origin.refs.master)  # set local "master" to track remote "master
+empty_repo.heads.master.checkout()  # checkout local "master" to working tree
+# Three above commands in one:
+empty_repo.create_head("master", origin.refs.master).set_tracking_branch(origin.refs.master).checkout()
+# rename remotes
+origin.rename("new_origin")
+# push and pull behaves similarly to `git push|pull`
+origin.pull()
+origin.push()  # attempt push, ignore errors
+origin.push().raise_if_error()  # push and raise error if it fails
+# assert not empty_repo.delete_remote(origin).exists()     # create and delete remotes
